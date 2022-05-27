a5f22b424083f70c6f44985a37865d4caf810e5a
maickol <maickolamador18@gmail.com>
2022-05-27 17:02:16 -0300

    SLD - Trello: "develop user context"
    - Changed get hook to the proper method

--- src/utils/hooks/get.js
+++ src/utils/hooks/get.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const Get = (route,config)=>{
-    return axios.put(`${process.env.NEXT_PUBLIC_API}/api/${route}`,{
+    return axios.get(`${process.env.NEXT_PUBLIC_API}/api/${route}`,{
+        //if they are set to true, will throw a cors error
+        //withCredentials: true,
         ...config
     })
 }
---
+++ src/utils/user/provider.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import Get from '@/utils/hooks/get'
+import jsCookie from 'js-cookie'
+import {toast} from 'react-toastify'
+
+const UserContext = createContext();
+
+function useUserContext() {
+    return useContext(UserContext);
+}
+
+function UserWrapper({ children, state }) {
+    const [user, setUser] = useState(false)
+
+    useEffect(()=>{
+        const token = jsCookie.get("sldtoken")
+        if (token) {
+            Get("user/me",{
+                headers:{
+                    sldtoken: token
+                }
+            }).then(res =>{
+                setUser(res.data)
+            })
+            .catch(err=>{
+                if (err.response) {
+                    return toast(err.response.data.msg)
+                }
+                return toast("hubo un error con tu token")
+            })
+        }
+    },[state])
+
+    return (
+        <UserContext.Provider value={user}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+
+export default UserWrapper;
+
+export {
+    useUserContext,
+    UserWrapper
+}
