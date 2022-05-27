d65ca0e6a7739bd461dc12cfa063fccdef44c37b
maickol <maickolamador18@gmail.com>
2022-05-27 20:02:21 -0300

    SLD - Trello: "Design user avatar menu"
    - Popover component developed

--- src/components/modules/user/avatar/index.jsx
+++ src/components/modules/user/avatar/index.jsx
@@ -1,11 +1,46 @@
-import {useUserContext} from "@/utils/user/provider"
+import { useUserContext } from "@/utils/user/provider"
 import Icon from "@/src/components/ui/icons"
+import PopOver from '@/ui/popovers'
+import Card from "@/ui/cards";
+import Button from '@/ui/buttons';
+import { useState } from "react";
 
-const UserAvatar = ()=>{
+const UserAvatar = () => {
+    const [isOpen, setOpen] = useState(false)
+
+
+    const ListItem = ({ icon, text, ...htmlProps }) => {
+        return (
+            <a className="list-group-item list-group-item-action border-0 rounded-12 my-1 d-flex pointer" 
+            {...htmlProps}>
+                {icon && <Icon id={icon} className="me-1" />}
+                {text}
+            </a>
+        )
+    }
     return (
-        <div className="bg-white rounded-12 d-flex flex-column justify-content-center px-2 pointer">
-            <Icon id="person" className="text-dark"/>
-        </div>
+
+        <PopOver isOpen={isOpen} content={
+            <Card className="p-2 mt-1 shadow">
+                <div className="list-group border-0">
+                    <ListItem icon="person" text="Mi perfil" />
+                    {
+                        !useUserContext().isSeller &&
+                        <>
+                        <ListItem icon="subscriptions" text="Suscripción" />
+                        <ListItem icon="store" text="Mis puestos" />
+                        <ListItem icon="add_business" text="Reclamar puesto" />
+                        </>
+                    }
+                    <ListItem icon="support_agent" text="Servicio al cliente" />
+                    <ListItem icon="logout" text="Cerrar sesión"  />
+                </div>
+            </Card>
+        }>
+            <Button color="white" className="px-2" onClick={() => setOpen(!isOpen)}>
+                <Icon id="person" className="text-dark" />
+            </Button>
+        </PopOver>
     )
 }
 
