f2b134b0cee19f7dd3582cae6f3fe9ef7d2ceb2a
maickol <maickolamador18@gmail.com>
2022-05-27 17:37:31 -0300

    SLD - Trello: "connect api signin with frontend"
    - Auto redirect to previous page in both auth method

--- src/components/modules/user/auth/isAuth.jsx
+++ src/components/modules/user/auth/isAuth.jsx
@@ -3,13 +3,16 @@ import Button from "@/ui/buttons"
 import Text from "@/ui/texts"
 import Link from "next/link"
 import {useUserContext} from "@/utils/user/provider"
+import {useRouter} from 'next/router'
 
 const IsAuth = () => {
+    const router = useRouter()
     if (useUserContext()) {
         return null
     }
+  
     return (
-        <Link href="/./user/auth/signin" passHref>
+        <Link href={`/./user/auth/signin?redirect=${router.asPath}`} passHref>
             <a className="text-decoration-none">
                 <Button color="white">
                     <Text className="d-none d-sm-block">
