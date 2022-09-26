a5d373486d4ae8c55d9039ea9a19798609dfdeea
maickol <maickolamador18@gmail.com>
2022-09-26 19:42:51 -0300

    img cloudinary fixed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true, //Will run twice useEffect, with that been said, userProvider is going to request twice user/me
   images: {
     loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/lightweightpipeline/'
+    path: 'https://res.cloudinary.com/saladapp/'
    }
 }
