edc2427bdde2b15ae1d3088183b709238a7bf23b
Nazareno Bregante <tobiasbregante2018@gmail.com>
2022-11-18 14:28:00 -0300

    vercel analytics added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,17 @@
 import '@/styles/build.scss'
 import LightTheme from '@/styles/themes/light'
 import { NextUIProvider } from '@nextui-org/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
+    <>
     <NextUIProvider theme={LightTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
+    <Analytics />
+    </>
   );
 }
 
