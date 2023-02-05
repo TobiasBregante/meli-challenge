9526b2370ca044c5d39368cfdfbe9b1c60d0f511
Nazareno Bregante <tobiasbregante2018@gmail.com>
2023-02-05 13:25:13 -0300

    ads added x2

--- src/components/modules/products/ads/modals.jsx
+++ src/components/modules/products/ads/modals.jsx
@@ -5,7 +5,7 @@ const AdsModals = ({ img, link }) => {
     return (
         <Modal open={true} >
             <a href={link} target='_blank'>
-                <Image src={img} layout='responsive' width={1000} height={1000} objectFit='contain'/>
+                <Image src={img} alt='ads' layout='responsive' width={1000} height={1000} objectFit='contain'/>
             </a>
         </Modal>
     )
