ede6c5e6b82d1524da4d26e9dc8b1f694a548305
Nazareno Bregante <tobiasbregante2018@gmail.com>
2023-06-04 19:00:44 -0300

    SA-67-Colors-Palette colores changed

--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -3,12 +3,23 @@ const lightTheme = createTheme({
     type: "light",
     theme: {
         colors: {
-            primary: '#D90429',
+            primary: '#249B3E',
             secondary: '#EF233C',
             thertiary: '#EDF2F4',
             blue: '#2B2D42',
             blue_light: '#8D99AE',
-            whatsapp: "#25d366"
+            whatsapp: "#25d366",
+            text: '#666666',
+            heading: '#222222',
+            light: '#888888',
+            border: '#EDEDED',
+            background1: '#F5F5F5',
+            background2: '#F6F6FC',
+            background3: '#F3F9FB',
+            yellow1: '#E3BC01',
+            yellow2: '#E3BC01',
+            white: '#FFFFFF',
+            line: '#D9D9D9',
         },
     }
 })
