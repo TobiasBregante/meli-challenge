a91f104fedc34487ddede4d53a82799a8d93bce3
Tobias Bregante <tobiasbregante2018@gmail.com>
2024-05-20 22:42:20 -0300

    url server changed x5

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 //const destinationHost = "https://www.iwarket.com"
-const destinationHost = 'https://iwarket.netlify.app'
+//const destinationHost = 'https://iwarket.netlify.app'
+//const destinationHost = 'http://localhost:3000'
+const destinationHost = 'https://iwarket-front.vercel.app'
 
 const domains = {
   argentina: "ar",
@@ -36,17 +38,19 @@ module.exports = {
   env: {
     ACCESS_TOKEN_MP: 'APP_USR-4128705031394975-102715-7eb2250f00d197dce296703c08a194de-348846213',
     //ACCESS_TOKEN_MP: 'TEST-4128705031394975-102715-1fcc9bea265dc1b7d8cb133ee1b1cf52-348846213',
+    NEXT_PUBLIC_API: 'https://iwarket-front.vercel.app/api',
     //NEXT_PUBLIC_API: 'http://localhost:3000/api',
     //NEXT_PUBLIC_API: 'https://www.iwarket.com/api',
-    NEXT_PUBLIC_API: 'https://iwarket.netlify.app/api',
+    //NEXT_PUBLIC_API: 'https://iwarket.netlify.app/api',
     MONGODB_KEY: 'mongodb+srv://nezzed:SfMzJUMxpuGZUZ64@ecommerce.9mipi43.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce',
     CLOUDINARY_API_SECRET: 'tC5yHp3VZH6VaLH4ObPo0AaijUM',
     CLOUDINARY_API_KEY: '279418967459214',
     CLOUDINARY_CLOUD_NAME: 'edata',
     JWT_USER_AUTH_KEY: 'ncajnvabcajaksvcacavaasfagahjaikcaklsaljcla',
-    FRONTEND_HOST: 'https://iwarket.netlify.app'
+    //FRONTEND_HOST: 'https://iwarket.netlify.app'
     //FRONTEND_HOST: 'https://www.iwarket.com',
-    //FRONTEND_HOST: 'http://localhost:3000'
+    //FRONTEND_HOST: 'http://localhost:3000',
+    FRONTEND_HOST: 'https://iwarket-front.vercel.app'
   },
   experimental: {
     serverActions: true,
@@ -81,7 +85,7 @@ module.exports = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://iwarket.netlify.app'
+            value: 'https://iwarket-front.vercel.app https://iwarket.netlify.app http://localhost:3000'
             //value: 'https://iwarket.com https://www.iwarket.com'
           }
         ]
